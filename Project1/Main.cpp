6a47acc0dbe9e833ec6fa911b7e7de16caad125d
Narayan-21 <naaidjan.19@gmail.com>
2024-03-30 11:12:33 +0530

    Created a new file - Log

--- Project1/Main.cpp
+++ Project1/Main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 
+
+/// cout: console output
+/// endl : end line
+/// cin.get() : wait for us to press ENTER to move to the next line of the program
+/// By default, main() returns 0
+/// Below is the declaration of the function defined in the another file.
+void Log(const char* message);
+
 int main()
 {
-	std::cout << "Hello world" << std::endl;
+	Log("Hello World");
 	std::cin.get();
 }
